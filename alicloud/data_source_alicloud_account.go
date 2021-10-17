29de660e612f20c6091551677d67cc973bebc61f
wanghx0991 <wanghx0991@outlook.com>
2021-10-17 11:24:22 +0800

    save

--- alicloud/data_source_alicloud_account.go
+++ alicloud/data_source_alicloud_account.go
@@ -11,6 +11,10 @@ func dataSourceAlicloudAccount() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceAlicloudAccountRead,
 
+
+
+
+
 		Schema: map[string]*schema.Schema{
 			// Computed values
 			"id": {
