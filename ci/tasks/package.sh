d3b04fd050f8c5c196d656b52e9690c6872fef8a
wanghx <wanghx0991@outlook.com>
2021-11-13 22:58:22 +0800

    save

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,6 +2,7 @@
 
 set -e -o pipefail
 cd terraform-provider-alicloud
+go get golang.org/x/tools/cmd/goimports
 #go env -w GOPROXY=https://goproxy.cn,direct
 #GOOS=linux GOARCH=amd64 go build -o bin/terraform-provider-alicloud
 make dev
