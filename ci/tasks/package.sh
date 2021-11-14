8fa4ee36352c74ac31c0180b64e868f4f7be54ec
wanghx <wanghx0991@outlook.com>
2021-11-14 13:52:42 +0800

    save

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -4,10 +4,10 @@ set -e -o pipefail
 cd terraform-provider-alicloud
 go get golang.org/x/tools/cmd/goimports
 #go env -w GOPROXY=https://goproxy.cn,direct
-#GOOS=linux GOARCH=amd64 go build -o bin/terraform-provider-alicloud
+
 echo "Starting Go mod tidy"
 go mod tidy
 echo "Ending Go mod tidy"
-make devlinux
+go build -o bin/terraform-provider-alicloud
 cd bin/
 ls -al
