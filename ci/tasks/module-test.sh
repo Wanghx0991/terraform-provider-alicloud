93a2351a6ea57ec7ee6a06946dfa9c0f3e18ff62
Wanghx0991 <67899003+Wanghx0991@users.noreply.github.com>
2021-11-17 09:01:01 +0800

    Update module-test.sh

--- ci/tasks/module-test.sh
+++ ci/tasks/module-test.sh
@@ -35,13 +35,13 @@ wget -qN https://releases.hashicorp.com/terraform/${terraform_version}/terraform
 unzip -o terraform_${terraform_version}_linux_amd64.zip -d /usr/bin
 
 pushd ${TERRAFORM_SOURCE_PATH}
-cp "${TERRAFORM_TEST_PATH}" ${TERRAFORM_SOURCE_PATH}
+cp -r "${TERRAFORM_TEST_PATH}" ${TERRAFORM_SOURCE_PATH}
 ls -al ${TERRAFORM_SOURCE_PATH}
 diffFiles=$(git diff --name-only HEAD~ HEAD)
 
 
 terraform init || exit 1
-
+terraform apply --auto-approve
 
 
 for fileName in ${diffFiles[*]};
