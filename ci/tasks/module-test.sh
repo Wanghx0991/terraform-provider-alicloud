9d09b3ac833a4ab0f39f8ec584dc1ad0dbbdfdbe
wanghaoxin.whx <wanghaoxin.whx@alibaba-inc.com>
2021-11-27 18:16:23 +0800

    save

--- ci/tasks/module-test.sh
+++ ci/tasks/module-test.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-: ${ALICLOUD_ACCESS_KEY:?}
-: ${ALICLOUD_SECRET_KEY:?}
-: ${ALICLOUD_REGION:?}
-: ${terraform_version:?}
+: "${ALICLOUD_ACCESS_KEY:?}"
+: "${ALICLOUD_SECRET_KEY:?}"
+: "${ALICLOUD_REGION:?}"
+: "${terraform_version:?}"
+: "${Stage:?}"
 
 export ALICLOUD_ACCESS_KEY=${ALICLOUD_ACCESS_KEY}
 export ALICLOUD_SECRET_KEY=${ALICLOUD_SECRET_KEY}
@@ -22,7 +23,6 @@ mkdir -p $TF_PLUGIN_CACHE_DIR
 export TF_PLUGIN_CACHE_DIR=${TF_PLUGIN_CACHE_DIR}
 
 TERRAFORM_SOURCE_PATH=$CURRENT_PATH/terraform-provider-alicloud
-TERRAFORM_TEST_PATH=$CURRENT_PATH/terraform_module_test
 TF_NEXT_PROVIDER=$CURRENT_PATH/next-provider/terraform-provider-alicloud
 
 
@@ -31,30 +31,29 @@ wget -qN https://releases.hashicorp.com/terraform/${terraform_version}/terraform
 unzip -o terraform_${terraform_version}_linux_amd64.zip -d /usr/bin
 
 pushd ${TERRAFORM_SOURCE_PATH}
-cp "${TERRAFORM_TEST_PATH}" ${TERRAFORM_SOURCE_PATH}
+
 ls -al
 diffFiles=$(git diff --name-only HEAD~ HEAD)
 
+aliyun oss cp oss://terraform-ci/ProviderVersion/terraform_test_linux.tgz ./terraform_test_linux.tgz --access-key-id ${ALICLOUD_ACCESS_KEY} --access-key-secret ${ALICLOUD_SECRET_KEY} --region cn-beijing
+ls -al
+unzip -o terraform_test_linux.tgz
+#unzip -o terraform_test_linux.tgz -d /usr/bin
 
-terraform init || exit 1
-
-
-
-for fileName in ${diffFiles[*]};
-do
-  if [[ ${fileName} == "alicloud/resource_alicloud"* || ${fileName} == "alicloud/data_source_alicloud"* ]];then
-    if [[ ${fileName} == *?_test.go ]]; then
-        echo -e "\033[33m[SKIPPED]\033[0m skipping the file $fileName, continue..."
-        continue
-    fi
-    echo "${fileName}"
-    fileName=(${fileName//\.go/_test\.go })
-    checkResourceName=$(grep "resourceId := \"alicloud_.*.default\""  ${fileName} | grep -Eo 'alicloud[a-z_]*'| head -n +1)
-    echo -e "\033[33m[Info]\033[0m file name = ${fileName} Resource Name = ${checkResourceName}"
-    cd "${TERRAFORM_TEST_PATH}" || exit
-    make build || exit
-    chmod +rx ./bin/terraform_test || exit
-    chmod +rx ./scripts/module.sh  || exit
-    ./bin/terraform_test  module_test -r="${checkResourceName}" || exit
-  fi
-done
+#for fileName in ${diffFiles[*]};
+#do
+#  if [[ ${fileName} == "alicloud/resource_alicloud"* || ${fileName} == "alicloud/data_source_alicloud"* ]];then
+#    if [[ ${fileName} == *?_test.go ]]; then
+#        echo -e "\033[33m[SKIPPED]\033[0m skipping the file $fileName, continue..."
+#        continue
+#    fi
+#    echo "${fileName}"
+#    fileName=(${fileName//\.go/_test\.go })
+#    checkResourceName=$(grep "resourceId := \"alicloud_.*.default\""  ${fileName} | grep -Eo 'alicloud[a-z_]*'| head -n +1)
+#    echo -e "\033[33m[Info]\033[0m file name = ${fileName} Resource Name = ${checkResourceName}"
+#    make build || exit
+#    chmod +rx ./bin/terraform_test || exit
+#    chmod +rx ./scripts/module.sh  || exit
+#    ./bin/terraform_test  module_test -r="${checkResourceName}" || exit
+#  fi
+#done
