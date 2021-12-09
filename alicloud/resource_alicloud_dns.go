5c14f3a66bae6b3823bb38d2ff59319c929baa5f
wanghaoxin.whx <wanghaoxin.whx@alibaba-inc.com>
2021-12-09 20:38:56 +0800

    save

--- alicloud/resource_alicloud_dns.go
+++ alicloud/resource_alicloud_dns.go
@@ -60,7 +60,7 @@ func resourceAlicloudDnsCreate(d *schema.ResourceData, meta interface{}) error {
 	request.RegionId = client.RegionId
 	request.DomainName = d.Get("name").(string)
 	request.ResourceGroupId = d.Get("resource_group_id").(string)
-	fmt.Println("111211")
+	fmt.Println("1112111")
 	raw, err := client.WithDnsClient(func(dnsClient *alidns.Client) (interface{}, error) {
 		return dnsClient.AddDomain(request)
 	})
