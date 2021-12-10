0fc4083d8d78280bd0f76f97e851bc5c491f5d43
wanghaoxin.whx <wanghaoxin.whx@alibaba-inc.com>
2021-12-10 11:30:44 +0800

    选择走diff方式区分schema

---
+++ scripts/field_check1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+CurrentPath="$(pwd)"
+
+pushd "${CurrentPath}"
+
+
+error=false
+
+diffFiles=$(git diff --name-only HEAD~ HEAD)
+for fileName in ${diffFiles[@]};
+do
+    if [[ ${fileName} == "alicloud/resource_alicloud"* ]];then
+        if [[ ${fileName} == *?_test.go ]]; then
+            echo -e "\033[33m[SKIPPED]\033[0m skipping the file $fileName, continue..."
+            continue
+        fi
+        resourceName=$(echo ${fileName} | grep -Eo "alicloud_[a-z_]*") || exit 1
+        echo -e "\033[33mThe ResourceName = ${resourceName}"
+        go test -v ./scripts/field_check_test.go -resource="${resourceName}"
+        if [[ "$?" == "1" ]]; then
+          echo -e "\033[31m ${resourceName}: Compatibility Error! Please check out the correct schema type \033[0m"
+          error=true
+        fi
+
+    fi
+done
+
+
+if $error; then
+  exit 1
+fi
+exit 0
