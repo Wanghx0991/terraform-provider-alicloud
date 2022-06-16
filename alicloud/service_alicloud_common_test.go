b19558cd347388ef9bf59b25de381f737e4a1235
wanghaoxin.whx <wanghaoxin.whx@alibaba-inc.com>
2022-06-16 19:18:17 +0800

    update region distrect

--- alicloud/service_alicloud_common_test.go
+++ alicloud/service_alicloud_common_test.go
@@ -2,6 +2,7 @@ package alicloud
 
 import (
 	"fmt"
+	"os"
 	"reflect"
 	"regexp"
 	"strings"
@@ -88,6 +89,7 @@ type resourceAttr struct {
 }
 
 func resourceAttrInit(resourceId string, checkMap map[string]string) *resourceAttr {
+	fmt.Printf("Current Region = %s", os.Getenv("ALICLOUD_REGION"))
 	if checkMap == nil {
 		checkMap = make(map[string]string)
 	}
